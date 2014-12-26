16bcf7fa04403d09699181808a657b53527411b5
Jared Smith <jared.rit@gmail.com>
2014-12-26 01:18:16 -0500

    Initial commit.

---
+++ mapview-tutorial/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  mapview-tutorial
+//
+//  Created by Jared Smith on 12/24/14.
+//  Copyright (c) 2014 tutorial. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
